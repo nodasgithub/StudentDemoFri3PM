f5a94632b351e8b8a09f43f413f77897b382f78d
nodasgithub <nodas@sheridan.desire2learn.com>
2019-02-01 16:12:40 -0500

    commiting Feb 1 version of my StudentDemo code

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
@@ -15,6 +15,7 @@ public class StudentDemo
   private String name;
   private String program;
   private String course;
+  private int i;
   
   /**
    * A constructor to pass in the given ID and then name
